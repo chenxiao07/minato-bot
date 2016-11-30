39d609b121ce29743440f86dc8747a6828f4f327
CHEN XIAO <xiao.chen@linecorp.com>
2016-11-30 17:22:58 +0900

    refactoring

--- hook.php
+++ hook.php
@@ -7,21 +7,15 @@ $json_object = json_decode($json_string);
 
 foreach ($json_object->events as $event) {
     if('message' == $event->type){
-        api_post_request($event->replyToken, $event->message->text);
+        reply_message($event->replyToken, $event->message->text);
     }
 }
 
-function api_post_request($token, $message) {
-    $url = 'https://api.line.me/v2/bot/message/reply';
-    $channel_access_token = 'P5pLeFX5jRoU9l9NNGPDDbceTn92PiKdIb/rrB9U6ecfQKTT67W2q5GCnEgH66whzuxb3yzfbLdecax3sMtzWkBY9cYBmt+NvU7DfOJ19rEFI0Mz5vtGabhp0EanclclCgMvvZT9ydvHnYl0JDKvWwdB04t89/1O/w1cDnyilFU=';
-    $headers = array(
-        'Content-Type: application/json',
-        "Authorization: Bearer {$channel_access_token}"
-    );
-
+function qr_code ($txt)
+{
     $name = uniqid(rand()).".png";
 
-    QRcode::png($message, $name);
+    QRcode::png($txt, $name);
 
     $post = array(
         'replyToken' => $token,
@@ -33,6 +27,19 @@ function api_post_request($token, $message) {
             )
         )
     );
+
+    return $post;
+}
+
+function reply_message($token, $txt) {
+    $url = 'https://api.line.me/v2/bot/message/reply';
+    $channel_access_token = 'P5pLeFX5jRoU9l9NNGPDDbceTn92PiKdIb/rrB9U6ecfQKTT67W2q5GCnEgH66whzuxb3yzfbLdecax3sMtzWkBY9cYBmt+NvU7DfOJ19rEFI0Mz5vtGabhp0EanclclCgMvvZT9ydvHnYl0JDKvWwdB04t89/1O/w1cDnyilFU=';
+    $headers = array(
+        'Content-Type: application/json',
+        "Authorization: Bearer {$channel_access_token}"
+    );
+
+    $post = qr_code($txt);
 
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_POST, true);
