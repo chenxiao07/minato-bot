77af1e5eddeea040585457d62f99a5a5298090fe
CHEN XIAO <xiao.chen@linecorp.com>
2016-12-05 14:16:18 +0900

    button template

--- hook.php
+++ hook.php
@@ -62,6 +62,79 @@ function button ($token, $txt)
     return $post;
 }
 
+function confirm_template ($token, $txt)
+{
+    $post = array(
+        'replyToken' => $token,
+        'messages' => array(
+            array(
+                'type' => 'template',
+                'altText' => 'this is a buttons template',
+                'template' => array(
+                    'type' => 'confirm',
+                    'text' => 'Are you sure?',
+                    'actions' => array(
+                        array(
+                            'type' => 'message',
+                            'label' => 'Yes',
+                            'text' => 'yes'
+                        ),
+                        array(
+                            'type' => 'message',
+                            'label' => 'No',
+                            'text' => 'no'
+                        )
+                    )
+                )
+            )
+        )
+    );
+
+    return $post;
+}
+
+function button_template ($token, $txt)
+{
+    $post = array(
+        'replyToken' => $token,
+        'messages' => array(
+            array(
+                'type' => 'buttons',
+                'altText' => 'this is a buttons template',
+                'template' => array(
+                    'type' => 'confirm',
+                    'title' => 'Button title',
+                    'text' => 'Button text test?',
+                    'actions' => array(
+                        array(
+                            'type' => 'message',
+                            'label' => 'button 01',
+                            'text' => 'button 01'
+                        ),
+                        array(
+                            'type' => 'message',
+                            'label' => 'button 02',
+                            'text' => 'button 02'
+                        ),
+                        array(
+                            'type' => 'message',
+                            'label' => 'button 03',
+                            'text' => 'button 03'
+                        ),
+                        array(
+                            'type' => 'message',
+                            'label' => 'button 04',
+                            'text' => 'button 04'
+                        )
+                    )
+                )
+            )
+        )
+    );
+
+    return $post;
+}
+
 function reply_message($token, $txt) {
     $url = 'https://api.line.me/v2/bot/message/reply';
     $channel_access_token = 'P5pLeFX5jRoU9l9NNGPDDbceTn92PiKdIb/rrB9U6ecfQKTT67W2q5GCnEgH66whzuxb3yzfbLdecax3sMtzWkBY9cYBmt+NvU7DfOJ19rEFI0Mz5vtGabhp0EanclclCgMvvZT9ydvHnYl0JDKvWwdB04t89/1O/w1cDnyilFU=';
@@ -70,7 +143,7 @@ function reply_message($token, $txt) {
         "Authorization: Bearer {$channel_access_token}"
     );
 
-    $post = button($token, $txt);
+    $post = button_template($token, $txt);
 
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_POST, true);
